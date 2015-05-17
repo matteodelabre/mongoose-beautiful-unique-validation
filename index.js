852188a88a84c63cf6861662f76e0b3ffba6793b
MattouFP <matteo.delabre@outlook.fr>
2015-05-17 13:08:59 +0200

    :lipstick: Add .eslintrc and fix coding style

--- index.js
+++ index.js
@@ -14,36 +14,36 @@ var regex = /index:\s*.+?\.\$(\S*)\s*dup key:\s*\{.*?:\s*"(.*)"\s*\}/;
  * @return {null}
  */
 function beautify(err, collection, map, callback) {
-    var matches = regex.exec(err.message), map;
+    var matches = regex.exec(err.message), props;
     
     if (matches && typeof matches[1] === 'string') {
-        collection.indexInformation(function (err, indexes) {
+        collection.indexInformation(function (dbErr, indexes) {
             var valError = new MongooseError.ValidationError(err),
                 index = indexes && indexes[matches[1]];
             
-            if (!err && index) {
+            if (!dbErr && index) {
                 // populate validation error with the fields
                 // contained in the index
                 index.forEach(function (item) {
-                    map = {
+                    props = {
                         type: 'Duplicate value',
                         path: item[0],
                         value: matches[2]
                     };
                     
-                    if (typeof map[item[0]] === 'string') {
-                        map.message = map[item[0]];
+                    if (typeof props[item[0]] === 'string') {
+                        props.message = props[item[0]];
                     }
                     
                     valError.errors[item[0]] =
-                        new MongooseError.ValidatorError(map);
+                        new MongooseError.ValidatorError(props);
                 });
                 
                 callback(valError);
                 return;
             }
             
-            callback(err);
+            callback(dbErr);
         });
     }
 }
@@ -62,10 +62,10 @@ module.exports = function (schema) {
         }
     }
     
-	schema.methods.trySave = function (callback) {
+    schema.methods.trySave = function (callback) {
         var collection = this.collection;
         
-		this.save(function (err, doc) {
+        this.save(function (err, doc) {
             // we have a native E11000 error, lets beautify it
             if (err && err.name === 'MongoError' && err.code === 11000) {
                 beautify(err, collection, map, function (newErr) {
