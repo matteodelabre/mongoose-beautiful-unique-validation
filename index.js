cb4d8ea493d363ce7ab8e632cbf7863f73d78f82
Mattéo Delabre <contact@matteodelabre.me>
2016-03-17 17:30:06 +0100

    :lipstick: Rewrite main code using Promises for clarity

--- index.js
+++ index.js
@@ -4,51 +4,83 @@ var MongooseError = require('mongoose/lib/error');
 var Promise = require('promise');
 
 var errorRegex = /index:\s*.+?\.\$(\S*)\s*dup key:\s*\{(.*?)\}/;
+var indexesCache = {};
 
 /**
- * Beautifies an E11000 or 11001 (unique constraint fail) Mongo error
- * by turning it into a validation error
+ * Retrieve index information using collection#indexInformation
+ * or previously cached data
  *
- * @param {MongoError} err Error to beautify
- * @param {Object} collection Collection for the associated model
- * @param {Object} map Map fields -> unique error messages
- * @param {function} callback Called with the beautified error
- * @return {null}
+ * @param {Object} collection Mongoose collection
+ * @return {Promise} Resolved with index information data
  */
-function beautify(err, collection, map, callback) {
-    var matches = errorRegex.exec(err.message),
-        indexName = matches[1], rawValues = matches[2].trim() + ',',
-        valueRegex = /\s*:\s*(\S*),/g;
+function getIndexes(collection) {
+    return new Promise(function (resolve, reject) {
+        if (indexesCache[collection.name]) {
+            resolve(indexesCache[collection.name]);
+            return;
+        }
 
-    if (matches) {
-        // look for the index contained in the MongoDB error
         collection.indexInformation(function (dbErr, indexes) {
-            var createdError, index;
-
             if (dbErr) {
-                callback(dbErr);
+                reject(dbErr);
                 return;
             }
 
-            index = indexes[indexName];
+            indexesCache[collection.name] = indexes;
+            resolve(indexes);
+        });
+    });
+}
+
+/**
+ * Beautify an E11000 or 11001 (unique constraint fail) Mongo error
+ * by turning it into a validation error
+ *
+ * @param {MongoError} err Error to process
+ * @param {Object} collection Collection for the associated model
+ * @param {Object} messages Map fields to unique error messages
+ * @return {Promise} Resolved with the beautified error message
+ */
+function beautify(error, collection, messages) {
+    var matches, indexName, rawValues, valueRegex;
+
+    // get index name from the error message
+    matches = errorRegex.exec(error.message),
+    indexName = matches[1], rawValues = matches[2].trim() + ',';
+    valueRegex = /\s*:\s*(\S*),/g;
+
+    return new Promise(function (resolve, reject) {
+        if (!matches) {
+            reject(new Error(
+                'mongoose-beautiful-unique-validation error: ' +
+                'Unrecognized error pattern. The error has not ' +
+                'been processed.'
+            ));
+            return;
+        }
+
+        // look for the index contained in the MongoDB error
+        getIndexes(collection).then(function (indexes) {
+            var index = indexes[indexName];
 
             if (!index) {
-                callback(new Error(
+                reject(new Error(
                     'mongoose-beautiful-unique-validation error: ' +
                     'could not find index "' + indexName + '" among ' +
                     'the collection\'s indexes'
                 ));
+                return;
             }
 
-            createdError = new MongooseError.ValidationError(err);
+            var createdError = new MongooseError.ValidationError(error);
 
             // populate validation error with the index's fields
             index.forEach(function (item) {
                 var value = valueRegex.exec(rawValues)[1],
                     path = item[0], props;
 
-                // value is parsed directly from the error
-                // string. Try to guess the value type (in a basic way)
+                // the value is parsed directly from the error
+                // string. We try to guess the value type (in a basic way)
                 if (value[0] === '"' || value[0] === "'") {
                     value = value.substr(1, value.length - 2);
                 } else if (!isNaN(value)) {
@@ -61,30 +93,28 @@ function beautify(err, collection, map, callback) {
                     value: value
                 };
 
-                if (typeof map[path] === 'string') {
-                    props.message = map[path];
+                if (typeof messages[path] === 'string') {
+                    props.message = messages[path];
                 }
 
                 createdError.errors[item[0]] =
                     new MongooseError.ValidatorError(props);
             });
 
-            callback(createdError);
-        });
-    } else {
-        callback(err);
-    }
+            resolve(createdError);
+        }).catch(reject);
+    });
 }
 
 module.exports = function (schema) {
-    var tree = schema.tree, key, map = {};
+    var tree = schema.tree, key, messages = {};
 
-    // fetch error messages defined in the
-    // 'unique' field, and replace them with 'true'
+    // fetch error messages defined in the "unique" field,
+    // store them for later use and replace them with true
     for (key in tree) {
         if (tree.hasOwnProperty(key)) {
             if (tree[key] && tree[key].unique) {
-                map[key] = tree[key].unique;
+                messages[key] = tree[key].unique;
                 tree[key].unique = true;
             }
         }
@@ -93,41 +123,56 @@ module.exports = function (schema) {
     schema._indexes.forEach(function (index) {
         if (index[0] && index[1] && index[1].unique) {
             Object.keys(index[0]).forEach(function (indexKey) {
-                map[indexKey] = index[1].unique;
+                messages[indexKey] = index[1].unique;
             });
 
             index[1].unique = true;
         }
     });
 
+    /**
+     * Save the schema in the database, beautify any
+     * unique error produced in the process
+     *
+     * @param {function} [callback] Callback called with any error and the document
+     * @return {Promise} If no callback was provided, resolved with the
+     * document and fails if any error was produced
+     */
     schema.methods.trySave = function (callback) {
-        var that = this, collection = this.collection, proxyFn;
+        var that = this, collection = this.collection;
 
-        proxyFn = function (resolve, handler) {
-            return function (err, doc) {
+        // use a no-op callback by default
+        if (!callback) {
+            callback = function () {};
+        }
+
+        return new Promise(function (resolve, reject) {
+            that.save(function (err, document, numAffected) {
                 // we have a native E11000/11001 error, lets beautify it
                 if (err && err.name === 'MongoError' && (err.code === 11000 || err.code === 11001)) {
-                    beautify(err, collection, map, function (newErr) {
-                        handler(newErr, doc);
+                    beautify(err, collection, messages).then(function (beautifiedErr) {
+                        // successfully beautified the error
+                        reject(beautifiedErr);
+                        callback(beautifiedErr);
+                    }).catch(function (beautifyingErr) {
+                        // the error could not be beautified. Warn about
+                        // it and pass on the normal error
+                        console.warn(beautifyingErr);
+                        reject(err);
+                        callback(err);
                     });
                     return;
                 }
 
-                if (resolve != null && err == null) {
-                    resolve(doc);
+                if (err) {
+                    reject(err);
+                    callback(err);
                     return;
                 }
 
-                handler(err, doc);
-            };
-        };
-
-        if (typeof callback === 'function') {
-            this.save(proxyFn(null, callback));
-        } else {
-            return new Promise(function (resolve, reject) {
-                that.save(proxyFn(resolve, reject));
+                resolve(document);
+                callback(null, document, numAffected);
             });
-        }
+        });
     };
 };
