004fd5a1dfe8a0bcc28297fd4fc133c429d23e94
Mattéo Delabre ✏ <contact@matteodelabre.me>
2016-09-10 15:52:17 +0200

    Also filter findOneAndUpdate errors

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 'use strict';
 
 var MongooseError = require('mongoose/lib/error');
-var Promise = require('promise');
-
-var errorRegex = /index:\s*(?:.+?\.\$)?(.*?)\s*dup/;
-var indexesCache = {};
 
 /**
  * Check if given error is an unique error
@@ -18,97 +14,42 @@ function isUniqueError(err) {
 }
 
 /**
- * Retrieve index information using collection#indexInformation
- * or previously cached data
- *
- * @param {Object} collection Mongoose collection
- * @return {Promise} Resolved with index information data
- */
-function getIndexes(collection) {
-    return new Promise(function (resolve, reject) {
-        if (indexesCache[collection.name]) {
-            resolve(indexesCache[collection.name]);
-            return;
-        }
-
-        collection.indexInformation(function (dbErr, indexes) {
-            if (dbErr) {
-                reject(dbErr);
-                return;
-            }
-
-            indexesCache[collection.name] = indexes;
-            resolve(indexes);
-        });
-    });
-}
-
-/**
  * Beautify an E11000 or 11001 (unique constraint fail) Mongo error
  * by turning it into a validation error
  *
  * @param {MongoError} err Error to process
- * @param {NativeCollection} collection Collection associated to the document
  * @param {Object} messages Map fields to unique error messages
- * @return {Promise} Resolved with the beautified error message
+ * @return {ValidationError} Beautified error message
  */
-function beautify(error, collection, messages) {
-    return new Promise(function (resolve, reject) {
-        // get the index name and duplicated values
-        // from the error message (with a hacky regex)
-        var matches = errorRegex.exec(error.message);
+function beautify(error, messages) {
+    var valuesMap = ('getOperation' in error) ? error.getOperation() : {};
+    var createdError = new MongooseError.ValidationError();
 
-        if (!matches) {
-            reject(new Error(
-                'mongoose-beautiful-unique-validation error: ' +
-                'Unrecognized error pattern. The error has not ' +
-                'been processed.'
-            ));
+    // the index contains the field keys in the same order
+    // (hopefully) as in the original error message. Create a
+    // duplication error for each field in the index, using
+    // valuesMap to get the duplicated values
+    Object.keys(valuesMap).forEach(function (path) {
+        // ignore Mongoose internals
+        if (path.slice(0, 2) === '__') {
             return;
         }
 
-        var indexName = matches[1];
-        var valuesMap = error.getOperation() || {};
+        var props = {
+            type: 'Duplicate value',
+            path: path,
+            value: valuesMap[path]
+        };
 
-        // retrieve the index by name in the collection
-        // fail if we do not find such index
-        getIndexes(collection).then(function (indexes) {
-            var index = indexes[indexName];
+        if (typeof messages[path] === 'string') {
+            props.message = messages[path];
+        }
 
-            if (!index) {
-                reject(new Error(
-                    'mongoose-beautiful-unique-validation error: ' +
-                    'could not find index "' + indexName + '" among ' +
-                    'the collection\'s indexes'
-                ));
-                return;
-            }
-
-            var createdError = new MongooseError.ValidationError();
-
-            // the index contains the field keys in the same order
-            // (hopefully) as in the original error message. Create a
-            // duplication error for each field in the index, using
-            // valuesList to get the duplicated values
-            index.forEach(function (item) {
-                var path = item[0];
-                var props = {
-                    type: 'Duplicate value',
-                    path: path,
-                    value: valuesMap[path]
-                };
-
-                if (typeof messages[path] === 'string') {
-                    props.message = messages[path];
-                }
-
-                createdError.errors[path] =
-                    new MongooseError.ValidatorError(props);
-            });
-
-            resolve(createdError);
-        }).catch(reject);
+        createdError.errors[path] =
+            new MongooseError.ValidatorError(props);
     });
+
+    return createdError;
 }
 
 module.exports = function (schema) {
@@ -135,9 +76,9 @@ module.exports = function (schema) {
         }
     });
 
-    // this hook gets called after any save operation by Mongoose
-    // and filters unique errors
-    schema.post('save', function (error, doc, next) {
+    // this hook gets called after any save or update
+    // operation by Mongoose and filters unique errors
+    var postHook = function (error, doc, next) {
         // if the next() function is missing, this might by
         // a sign that we use an outdated Mongoose
         if (typeof next !== 'function') {
@@ -151,19 +92,14 @@ module.exports = function (schema) {
         }
 
         if (isUniqueError(error)) {
-            // we have a native E11000/11001 error, lets beautify it
-            beautify(error, doc.collection, messages).then(function (beautifulError) {
-                // successfully beautified the error
-                next(beautifulError);
-            }).catch(function (beautifyingErr) {
-                // the error could not be beautified. Warn about
-                // it and pass on the normal error
-                console.warn(beautifyingErr);
-                next(error);
-            });
+            // beautify unicity constraint failure errors
+            next(beautify(error, messages));
         } else {
-            // otherwise, just pass on the error
+            // pass over normal errors
             next(error);
         }
-    });
+    };
+
+    schema.post('save', postHook);
+    schema.post('findOneAndUpdate', postHook);
 };
