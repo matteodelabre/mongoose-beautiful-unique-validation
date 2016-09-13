488f7aae0175a9ab70687664eb86f5531fac6fef
Mattéo Delabre ✏️ <contact@matteodelabre.me>
2016-09-13 23:59:13 +0200

    Only raise sub-errors for fields that are duplicates (#30) (fixes #29)
    * Update tests to reflect #29
    
    * Fix last test
    
    * Report for duplicated fields only

--- tests/utils.js
+++ tests/utils.js
@@ -14,55 +14,65 @@ function wait(time) {
 }
 
 /**
- * Assert that saving the given document twice
- * produces a beautified error
+ * Assert that saving the given two documents containing
+ * duplicate fields throws a beautified duplicate error
  *
  * @param {Object} assert Tape assertion object
- * @param {function} creator Callback to create a document
- * @param {Object} doc The document to use
- * @param {string} [message] Also check that the given message is passed over
+ * @param {function} creator Callback for saving the documents
+ * @param {Object} doc1 The first document to save
+ * @param {Object} doc2 The second document to save
+ * @param {Array.<string>} dups The list of duplicated keys between the 2 docs
+ * @param {string} [message=default] Ensure the given message is
+ * thrown with the duplicate error's sub-errors
  */
-function assertDuplicateFailure(assert, creator, doc, message) {
-    // save the first instance
-    creator(doc).then(function () {
-        // ensure the unique index is rebuilt
+function assertDuplicateFailure(assert, creator, doc1, doc2, dups, message) {
+    // save the first document
+    creator(doc1).then(function () {
+        // wait to ensure the unique index is rebuilt
         return wait(500);
     }).then(function () {
-        // try to save a duplicate (should not work)
-        creator(doc).then(function () {
-            assert.fail('should not save duplicate successfully');
-            assert.end();
-        }, function (err) {
-            assert.ok(err, 'err should exist');
-            assert.equal(err.name, 'ValidationError', 'outer err should be of type ValidationError');
-
-            Object.keys(doc).forEach(function (key) {
-                var error = err.errors[key];
-                var value = error.properties.value;
-
-                // in case of buffers, only compare the inner "buffer" property
-                if (typeof value === 'object' && value !== null && value._bsontype === 'Binary') {
-                    value = value.buffer;
-                }
-
-                assert.equal(error.name, 'ValidatorError', 'inner err should be ValidatorError');
-                assert.equal(error.kind, 'Duplicate value', 'kind of err should be Duplicate value');
-
-                // check that our custom error message was passed down, if asked
-                if (message) {
-                    assert.equal(error.message, message, 'message should be our custom value');
-                }
-
-                assert.equal(error.properties.path, key, 'err should be on the correct path');
-                assert.equal(error.properties.type, 'Duplicate value');
-                assert.looseEqual(value, doc[key], 'err should contain the problematic value');
-            });
-
-            assert.end();
-        });
+        // try to save the duplicate document (should not work)
+        return creator(doc2);
     }, function (err) {
-        assert.error(err, 'should save original instance successfully');
+        assert.error(err, 'should save the first document successfully');
         assert.end();
+    }).then(function () {
+        assert.fail('should not save the second (duplicate) document successfully');
+        assert.end();
+    }, function (err) {
+        assert.equal(err.name, 'ValidationError', 'the thrown error should be of type ValidationError');
+
+        var missing = dups.filter(function (key) {
+            var suberr = err.errors[key];
+
+            if (!err.errors[key]) {
+                return true;
+            }
+
+            assert.equal(suberr.name, 'ValidatorError', 'each sub-error should be of type ValidatorError');
+            assert.equal(suberr.kind, 'Duplicate value', 'each sub-error\'s kind should be "Duplicate value"');
+
+            if (message !== undefined) {
+                assert.equal(suberr.message, message, 'each sub-error should carry over the custom message');
+            }
+
+            // with buffer values, only compare the inner "buffer" property
+            assert.equal(suberr.properties.value.toString(), doc2[key].toString(), 'the sub-error should contain the duplicated value');
+            assert.equal(suberr.properties.path, key, 'the sub-error should contain the duplicated value\'s path');
+            assert.equal(suberr.properties.type, 'Duplicate value', 'the sub-error\'s type should be "Duplicate value"');
+
+            return false;
+        });
+
+        assert.equal(missing.length, 0, 'should report a sub-error per duplicated field');
+        assert.equal(
+            Object.keys(err.errors).length, dups.length,
+            'should only report a sub-error per duplicated field'
+        );
+
+        assert.end();
+    }).catch(function (err) {
+        assert.error(err);
     });
 }
 
